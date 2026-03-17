35733888ccdd652ea146d2bb30a62ba5959042f0
Atharva Kailash Dhamale <atharvadhamale43@gmail.com>
2026-03-17 22:50:15 +0530

    Add files via upload

---
+++ Customized_Virtual_File_System/program550.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+//Conditional preprocessing
+int main()
+{
+    #ifdef _WIN32
+    system("cls");
+
+    #else
+    system("clear");
+
+    #endif
+
+    return 0;
+}
